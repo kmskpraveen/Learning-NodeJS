2925eefe2f9ec4b8a4ea3dcd937f44bf3194f9b4
Kolluru Malyadri Sai Krishna Praveen <praveen.k@COMAKEIT.COM>
2019-02-15 10:51:34 +0530

    commit_Updated file names

---
+++ 2_app.js
@@ -0,0 +1,41 @@
+// Run this code in Terminal through command: node app.js
+// ============================================================================
+// Code for videos 7,8,9,10
+// ============================================================================
+
+// Prototype Demo.
+ function User(){
+     this.name = "";
+     this.life = 100;
+     this.giveLife = function giveLife(targetPerson){
+        targetPerson.life += 1;
+        console.log(this.name+" gave life to "+targetPerson.name);
+     }
+ }
+
+ var Adam = new User();
+ var Richards = new User();
+
+Adam.name = "Adam";
+Richards.name = "Richards";
+
+Adam.giveLife(Richards)
+
+console.log("Life of "+Adam.name+": "+Adam.life);
+console.log("Life of "+Richards.name+": "+Richards.life);
+
+// Prototype gives us the ability to add methods, parameters to the objects in classes.
+User.prototype.injure = function injure(targetPerson){
+    targetPerson.life -= 3;
+    console.log(this.name+" injured "+targetPerson.name);
+}
+
+Adam.injure(Richards);
+
+console.log("Life of "+Adam.name+": "+Adam.life);
+console.log("Life of "+Richards.name+": "+Richards.life);
+
+User.prototype.coins = 100;
+
+console.log("Coins of "+Adam.name+": "+Adam.coins);
+console.log("Coins of "+Richards.name+": "+Richards.coins);
